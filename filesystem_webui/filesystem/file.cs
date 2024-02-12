b48cf3da57883f9d04a4822c5e79a60a4eae6c1c
Lars Saalbrink <larssaalbrink@gmail.com>
2024-02-12 12:13:21 +0100

    Getters, setters & error handling

--- filesystem_webui/filesystem/file.cs
+++ filesystem_webui/filesystem/file.cs
@@ -1,27 +1,101 @@
+using System;
 using System.IO;
 
 namespace Web_fileSystem
 {
     public class Web_file
     {
-        public string Name { get; private set; }
-        public string Path { get; private set; }
-        public long Size { get; private set; }
-        public Web_folder ParentFolder { get; private set; }
+        private string _name = "";
+        private string _path = "";
+        private long _size = 0;
+        private Web_folder? _parentFolder = null;
+        private bool _wasDeleted = false;
+
+        public string Name
+        {
+            get
+            {
+                CheckDeleted();
+                return _name;
+            }
+            private set
+            {
+                CheckDeleted();
+                _name = value;
+            }
+        }
+
+        public string Path
+        {
+            get
+            {
+                CheckDeleted();
+                return _path;
+            }
+            private set
+            {
+                CheckDeleted();
+                _path = value;
+            }
+        }
+
+        public long Size
+        {
+            get
+            {
+                CheckDeleted();
+                return _size;
+            }
+            private set
+            {
+                CheckDeleted();
+                _size = value;
+            }
+        }
+
+        public Web_folder? ParentFolder
+        {
+            get
+            {
+                CheckDeleted();
+                return _parentFolder;
+            }
+            private set
+            {
+                CheckDeleted();
+                _parentFolder = value;
+            }
+        }
 
         public Web_file(string filePath, Web_folder parentFolder)
         {
             var fileInfo = new FileInfo(filePath);
-            Name = fileInfo.Name;
-            Path = filePath;
-            Size = fileInfo.Length;
-            ParentFolder = parentFolder;
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException("The file does not exist.", filePath);
+            }
+            else
+            {
+                Name = fileInfo.Name;
+                Path = filePath;
+                Size = fileInfo.Length;
+                ParentFolder = parentFolder;
+            }
         }
 
         public void Delete()
         {
             File.Delete(Path);
-            Console.PrintLine("Deleted file: '" + Name + "'");
+            _wasDeleted = true;
+            Console.WriteLine("Deleted file: '" + Name + "'");
+        }
+
+        private void CheckDeleted()
+        {
+            if (_wasDeleted)
+            {
+                throw new InvalidOperationException("Cannot access a deleted file.");
+            }
         }
     }
 }
--- filesystem_webui/filesystem/folder.cs
+++ filesystem_webui/filesystem/folder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,41 +7,129 @@ namespace Web_fileSystem
 {
     public class Web_folder
     {
+        private bool _wasDeleted = false;
+        private string _name = "";
+        private string _path = "";
+        private List<Web_file>? _files = null;
+        private long _size = 0;
+        private Web_folder? _parentFolder = null;
+
         public const string root_name = "fs_content";  // Folder to use as root for the fs
-        public string Name { get; private set; }
-        public string Path { get; private set; }
-        public List<Web_file> Files { get; private set; }
-        public long Size { get; private set; }  //Bytes
-        public Web_folder? ParentFolder { get; private set; }
+        public string Name
+        {
+            get
+            {
+                CheckDeleted();
+                return _name;
+            }
+            private set
+            {
+                CheckDeleted();
+                _name = value;
+            }
+        }
+        public string Path
+        {
+            get
+            {
+                CheckDeleted();
+                return _path;
+            }
+            private set
+            {
+                CheckDeleted();
+                _path = value;
+            }
+        }
+        public List<Web_file>? Files
+        {
+            get
+            {
+                CheckDeleted();
+                return _files;
+            }
+            private set
+            {
+                CheckDeleted();
+                _files = value;
+            }
+        }
+        public long Size
+        {
+            get
+            {
+                CheckDeleted();
+                return _size;
+            }
+            private set
+            {
+                CheckDeleted();
+                _size = value;
+            }
+        }
+        public Web_folder? ParentFolder
+        {
+            get
+            {
+                CheckDeleted();
+                return _parentFolder;
+            }
+            private set
+            {
+                CheckDeleted();
+                _parentFolder = value;
+            }
+        }
 
         public Web_folder(string folderPath)
         {
             var directoryInfo = new DirectoryInfo(folderPath);
-            Name = directoryInfo.Name;
-            Path = folderPath;
+            if (!directoryInfo.Exists)
+            {
+                throw new DirectoryNotFoundException($"The directory {folderPath} does not exist.");
+            }
+            else
+            {
+                Name = directoryInfo.Name;
+                Path = folderPath;
 
-            // Limit access to within the fs folder for this project 
-            ParentFolder = (directoryInfo.Parent.Name == root_name) ? ParentFolder = null : ParentFolder = new Web_folder(directoryInfo.Parent.FullName);
+                // Limit access to within the fs folder for this project 
+                // #pragma warning disable CS8602  // If directoryInfo.Exists, this can never happen
+                ParentFolder = (directoryInfo?.Parent?.Name == root_name) ? ParentFolder = null : ParentFolder = new Web_folder(folderPath);
+                // #pragma warning restore CS8602
 
-            Files = Directory.GetFiles(folderPath)
-                             .Select(filePath => new Web_file(filePath, this))
-                             .ToList();
+                Files = Directory.GetFiles(folderPath)
+                                 .Select(filePath => new Web_file(filePath, this))
+                                 .ToList();
 
-            Size = Files.Sum(file => file.Size);
+                Size = Files.Sum(file => file.Size);
+            }
+
+
         }
 
         public Web_file? FindFile(string name)
         {
-            return Files.FirstOrDefault(file => file.Name == name);
+            CheckDeleted();
+            return Files?.FirstOrDefault(file => file.Name == name);
         }
 
         public void Delete_file(string name)
         {
-            Web_file file = FindFile(name);
+            CheckDeleted();
+            Web_file? file = FindFile(name);
             if (file != null)
             {
                 file.Delete();       // Delete on local fs
-                Files.Remove(file);  // Remove Files object from list of this folder
+                Files?.Remove(file);  // Remove Files object from list of this folder
+            }
+        }
+
+        private void CheckDeleted()
+        {
+            if (_wasDeleted)
+            {
+                throw new InvalidOperationException("Cannot access a deleted folder.");
             }
         }
     }
